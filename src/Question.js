bfef50d967145a9a0aecc9642ef36770fa61fe32
Tom Lou <louyu27@cs.washington.edu>
2019-12-29 21:00:39 -0800

    Show if the vote is locked

--- src/Question.js
+++ src/Question.js
@@ -37,6 +37,7 @@ class Question extends React.Component {
     return (
     <div className="card m-3">
       <div className="card-header">
+        {this.props.enable? "" : "[Locked] "}
         {this.props.message}
         {warning_element}
       </div>
