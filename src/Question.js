5f04b01b5a5c529f2badb3a7e50f6acfc195b2da
Tom Lou <louyu27@cs.washington.edu>
2019-12-29 18:20:35 -0800

    Fixed overflow

--- src/Question.js
+++ src/Question.js
@@ -9,7 +9,7 @@ class Question extends React.Component {
       let active = this.props.selected.includes(choice.id);
       choice_elements.push(
         <li key={choice.id}
-          className="list-group-item p-0">
+          className="list-group-item p-0 overflow-hidden">
             <button className={"btn btn-block rounded-0 m-0 " +
                 (active? "btn-primary" : "btn-light")}
                 disabled={!this.props.enable}>
