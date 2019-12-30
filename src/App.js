faeeff11760a0cdb6cfd55765cfcdace16acbbea
Tom Lou <louyu27@cs.washington.edu>
2019-12-29 17:15:13 -0800

    Display serial number

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import StatusBar from './StatusBar/StatusBar'
+import {useParams} from "react-router-dom";
 
 class App extends React.Component {
+
   render = () => {
+
     return (
-      <div className="App">
+      <div>
+        <nav className="navbar navbar-light bg-light sticky-top">
+          Your serial number: {this.props.match.params.serial_number}
+        </nav>
         <StatusBar ref={(ref) => {this.statusBar = ref;}}
           timeout={5000}/>
         <button onClick={()=>{this.statusBar.onRequestStart()}}>Submit</button>
